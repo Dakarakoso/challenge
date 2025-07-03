cc95e4c084a94c7eecf3d870650003b40169f473
Willian Maruyama <willianmaruyama@MacBookPro.elecom>
2025-07-03 10:31:38 +0900

    feat: add backup module and configure DR AWS provider credentials

--- infrastructure/environments/prod/providers.tf
+++ infrastructure/environments/prod/providers.tf
@@ -13,4 +13,9 @@ provider "aws" {
 provider "aws" {
   alias  = "dr_region"
   region = var.dr_region
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 }
